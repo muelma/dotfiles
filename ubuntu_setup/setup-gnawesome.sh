2b338238b0f45b73cbc329b534f0ec382cdf5b38
Marco Müller <muelma@gmail.com>
2013-11-07 19:00:02 +0100

    setup script for Ubuntu 13.10

--- ubuntu_setup/setup-gnawesome.sh
+++ ubuntu_setup/setup-gnawesome.sh
@@ -2,13 +2,11 @@
 cat << 'EOF' > /usr/share/gnome-session/sessions/awesome.session
 [GNOME Session]
 Name=Awesome session
-RequiredComponents=gnome-settings-daemon;
+RequiredComponents=awesome;gnome-settings-daemon;
 RequiredProviders=windowmanager;
 DefaultProvider-windowmanager=awesome
 DesktopName=GNOME
 EOF
-#RequiredProviders=windowmanager;notifications;
-#DefaultProvider-notifications=notification-daemon
 cat << 'EOF' > /usr/share/applications/awesome.desktop
 [Desktop Entry] 
 Encoding=UTF-8
@@ -18,21 +16,16 @@ Comment=Highly configurable framework window manager
 TryExec=awesome
 Exec=awesome
 NoDisplay=true
-X-GNOME-WMSettingsModule=awesome
-# name we put on the WM spec check window
-X-GNOME-WMName=Awesome
-X-GNOME-Autostart-Phase=WindowManager;Panel
-X-GNOME-Provides=windowmanager;panel
-X-GNOME-AutoRestart=true
-X-GNOME-Autostart-Notify=true
 EOF
-cat << 'EOF' > /usr/share/xsessions/gnome-awesome.desktop
+cat << 'EOF' > /usr/share/xsessions/awesome-gnome.desktop
 [Desktop Entry]
-Name=gnawesome
+Name=Awesome GNOME
 Comment=Dynamic window manager
-TryExec=/usr/bin/gnome-session
-Exec=gnome-session --session awesome
-Type=XSession
+Exec=gnome-session --session=awesome
+TryExec=awesome
+Type=Application
+X-LightDM-DesktopName=Awesome GNOME
+X-Ubuntu-Gettext-Domain=gnome-session-3.0
 EOF
 cp custom_awesome_badge.png /usr/share/unity-greeter/custom_gnome-awesome_badge.png
 echo "you might consider using gsetter.sh for disabling the desktop/icons"
