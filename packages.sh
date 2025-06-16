e93be79d1df706f8fca60a56f48c2146a2db4996
Marco Müller <muelma@gmail.com>
2025-06-16 15:31:50 +0200

    minor tweaks to formats, colors and packages

--- packages.sh
+++ packages.sh
@@ -1,3 +1,7 @@
 #/usr/bin/env bash
 sudo pacman -S libnotify dunst jq rofi-wayland dunst ttf-iosevka-nerd --needed --noconfirm
 yay -S sddm-theme-mountain sddm-terminal-inspired-theme-git multicolor-sddm-theme archlinux-themes-sddm --needed --noconfirm
+# neovim
+sudo pacman -S neovim ripgrep fd git make unzip gcc lua luarocks
+sudo pacman -S keepassxc cups-pdf cups
+sudo pacman -S fzf
